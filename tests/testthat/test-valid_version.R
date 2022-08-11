eb65784e822fdbbf16c65aef37b904c911e03153
chanyub Park <mrchypark@gmail.com>
2022-08-12 01:47:02 +0900

    add version list add more test test pass version

--- tests/testthat/test-valid_version.R
+++ tests/testthat/test-valid_version.R
@@ -1,4 +1,8 @@
 test_that("multiplication works", {
-  expect_equal(valid_version("v1.12.1"), TRUE)
-  expect_equal(valid_version("v1.13.1"), FALSE)
+  expect_equal(valid_version("1.12.1"), TRUE)
+  expect_equal(valid_version("1.13.1"), FALSE)
+  expect_equal(valid_version("1.0.0"), TRUE)
+  expect_equal(valid_version("iufdf"), FALSE)
+  expect_equal(valid_version("rtfv"), FALSE)
+  expect_equal(valid_version("v1.12.1"), FALSE)
 })
