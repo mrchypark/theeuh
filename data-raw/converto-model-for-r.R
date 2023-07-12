6f46bbaa5505215a633f5897828aa46eb289dbc1
Chanyub.Park <mrchypark@gmail.com>
2023-07-12 19:42:40 +0900

    doing padding

--- data-raw/converto-model-for-r.R
+++ data-raw/converto-model-for-r.R
@@ -5,10 +5,10 @@ MyModel <- nn_module(
   initialize = function(){
     self$embedding <- nn_embedding(num_embeddings=1951, embedding_dim=100)
     self$conv1 <- nn_conv1d(100, 128, 1)
-    self$conv2 <- nn_conv1d(100, 256, 2)
-    self$conv3 <- nn_conv1d(100, 128, 3)
-    self$conv4 <- nn_conv1d(100, 64, 4)
-    self$conv5 <- nn_conv1d(100, 32, 5)
+    self$conv2 <- nn_conv1d(100, 256, 2, padding = c(1,0))
+    self$conv3 <- nn_conv1d(100, 128, 3, padding = c(1,0))
+    self$conv4 <- nn_conv1d(100, 64, 4, padding = c(2,0))
+    self$conv5 <- nn_conv1d(100, 32, 5, padding = c(2,0))
     self$batch_norm <- nn_batch_norm1d(608)
     self$fc1 <- nn_linear(608, 300)
     self$fc2 <- nn_linear(300, 150)
@@ -17,11 +17,12 @@ MyModel <- nn_module(
   },
   forward = function(x){
     x <- self$embedding(x)
-    x1 <- nnf_relu(self$conv1(x))
-    x2 <- nnf_relu(self$conv2(x))
-    x3 <- nnf_relu(self$conv3(x))
-    x4 <- nnf_relu(self$conv4(x))
-    x5 <- nnf_relu(self$conv5(x))
+    xr <- torch_reshape(x, c(1,100,200))
+    x1 <- nnf_relu(self$conv1(xr))
+    x2 <- nnf_relu(self$conv2(xr))
+    x3 <- nnf_relu(self$conv3(xr))
+    x4 <- nnf_relu(self$conv4(xr))
+    x5 <- nnf_relu(self$conv5(xr))
     x <- torch_cat(list(x1, x2, x3, x4, x5), dim=2)
     x <- self$batch_norm(x)
     x <- nnf_relu(self$fc1(x))
@@ -34,6 +35,7 @@ MyModel <- nn_module(
 
 
 model <- MyModel()
+
 
 state_dict <- torch::load_state_dict("inst/model/kospacing2.pth")
 
@@ -59,3 +61,37 @@ state_dict$fc3.weight <- state_dict$fc3.weight$view(c(1, 50))
 model$load_state_dict(state_dict)
 
 
+
+
+torch_tensor(mat,dtype=torch_long()) %>%
+  modelp$embedding$forward() %>%
+  torch_reshape(c(1,100,200)) %>%
+  modelp$conv1() %>%
+  nnf_relu() -> c1
+
+torch_tensor(mat,dtype=torch_long()) %>%
+  modelp$embedding$forward() %>%
+  torch_reshape(c(1,100,200)) %>%
+  modelp$conv2() %>%
+  nnf_relu() -> c2
+
+torch_tensor(mat,dtype=torch_long()) %>%
+  modelp$embedding$forward() %>%
+  torch_reshape(c(1,100,200)) %>%
+  modelp$conv3() %>%
+  nnf_relu() -> c3
+
+torch_tensor(mat,dtype=torch_long()) %>%
+  modelp$embedding$forward() %>%
+  torch_reshape(c(1,100,200)) %>%
+  modelp$conv4() %>%
+  nnf_relu() -> c4
+
+torch_tensor(mat,dtype=torch_long()) %>%
+  modelp$embedding$forward() %>%
+  torch_reshape(c(1,100,200)) %>%
+  modelp$conv5() %>%
+  nnf_relu() -> c5
+
+
+torch_cat(list(c1,c2,c3,c4,c5), dim = 2L)
